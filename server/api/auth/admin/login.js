2d46692011209356d12f782180fbe4f828ed8b95
hayat-se <hayatlucky064@gmail.com>
2026-02-21 02:20:51 +0500

    Solving CORS 500 Error

--- server/api/auth/admin/login.js
+++ server/api/auth/admin/login.js
@@ -1,5 +1,15 @@
 const { adminLogin } = require('../../../../controllers/authController');
 
 module.exports = async (req, res) => {
+  // Handle CORS preflight
+  if (req.method === 'OPTIONS') {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.status(200).end();
+    return;
+  }
+
+  res.setHeader('Access-Control-Allow-Origin', '*');
   await adminLogin(req, res);
 };
